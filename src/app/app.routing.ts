8eb2ed9e4ddfb4ece1896cdef96020ef2ce83700
diditsadidnsm <diditsadidnsm180818@gmail.com>
2021-07-31 02:43:41 +0700

    new project angular zevitsoft

---
+++ src/app/app.routing.ts
@@ -0,0 +1,115 @@
+import { Route } from '@angular/router';
+import { AuthGuard } from 'app/core/auth/guards/auth.guard';
+import { NoAuthGuard } from 'app/core/auth/guards/noAuth.guard';
+import { LayoutComponent } from 'app/layout/layout.component';
+import { InitialDataResolver } from 'app/app.resolvers';
+
+// @formatter:off
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export const appRoutes: Route[] = [
+
+    // Redirect empty path to '/dashboards/project'
+    {path: '', pathMatch : 'full', redirectTo: 'dashboards/project'},
+
+    // Redirect signed in user to the '/dashboards/project'
+    //
+    // After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
+    // path. Below is another redirection for that path to redirect the user to the desired
+    // location. This is a small convenience to keep all main routes together here on this file.
+    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'dashboards/project'},
+
+    // Auth routes for guests
+    {
+        path: '',
+        canActivate: [NoAuthGuard],
+        canActivateChild: [NoAuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {path: 'confirmation-required', loadChildren: () => import('app/modules/auth/confirmation-required/confirmation-required.module').then(m => m.AuthConfirmationRequiredModule)},
+            {path: 'forgot-password', loadChildren: () => import('app/modules/auth/forgot-password/forgot-password.module').then(m => m.AuthForgotPasswordModule)},
+            {path: 'reset-password', loadChildren: () => import('app/modules/auth/reset-password/reset-password.module').then(m => m.AuthResetPasswordModule)},
+            {path: 'sign-in', loadChildren: () => import('app/modules/auth/sign-in/sign-in.module').then(m => m.AuthSignInModule)},
+            {path: 'sign-up', loadChildren: () => import('app/modules/auth/sign-up/sign-up.module').then(m => m.AuthSignUpModule)}
+        ]
+    },
+
+    // Auth routes for authenticated users
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {path: 'sign-out', loadChildren: () => import('app/modules/auth/sign-out/sign-out.module').then(m => m.AuthSignOutModule)},
+            {path: 'unlock-session', loadChildren: () => import('app/modules/auth/unlock-session/unlock-session.module').then(m => m.AuthUnlockSessionModule)}
+        ]
+    },
+
+    // Landing routes
+    {
+        path: '',
+        component  : LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children   : [
+            {path: 'home', loadChildren: () => import('app/modules/landing/home/home.module').then(m => m.LandingHomeModule)},
+        ]
+    },
+
+    // Admin routes
+    {
+        path       : '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component  : LayoutComponent,
+        resolve    : {
+            initialData: InitialDataResolver,
+        },
+        children   : [
+
+            // Dashboards
+            {path: 'dashboards', children: [
+                {path: 'project', loadChildren: () => import('app/modules/admin/dashboards/project/project.module').then(m => m.ProjectModule)},
+                {path: 'analytics', loadChildren: () => import('app/modules/admin/dashboards/analytics/analytics.module').then(m => m.AnalyticsModule)},
+            ]},
+
+            // Apps
+            {path: 'apps', children: [
+                {path: 'presale', loadChildren: () => import('app/modules/admin/apps/academy/academy.module').then(m => m.AcademyModule)},
+                {path: 'transaction', loadChildren: () => import('app/modules/admin/apps/contacts/contacts.module').then(m => m.ContactsModule)},
+                {path: 'help-center', loadChildren: () => import('app/modules/admin/apps/help-center/help-center.module').then(m => m.HelpCenterModule)},
+            ]},
+
+            // Pages
+            {path: 'pages', children: [
+                {path: 'presaleschedule', loadChildren: () => import('app/modules/admin/pages/activities/activities.module').then(m => m.ActivitiesModule)},
+                {path: 'authentication', loadChildren: () => import('app/modules/admin/pages/authentication/authentication.module').then(m => m.AuthenticationModule)},
+                {path: 'error', children: [
+                    {path: '404', loadChildren: () => import('app/modules/admin/pages/error/error-404/error-404.module').then(m => m.Error404Module)},
+                    {path: '500', loadChildren: () => import('app/modules/admin/pages/error/error-500/error-500.module').then(m => m.Error500Module)}
+                ]},
+                {path: 'invoice', children: [
+                    {path: 'printable', children: [
+                        {path: 'compact', loadChildren: () => import('app/modules/admin/pages/invoice/printable/compact/compact.module').then(m => m.CompactModule)},
+                        {path: 'modern', loadChildren: () => import('app/modules/admin/pages/invoice/printable/modern/modern.module').then(m => m.ModernModule)}
+                    ]}
+                ]},
+
+                // Settings
+                {path: 'settings', loadChildren: () => import('app/modules/admin/pages/settings/settings.module').then(m => m.SettingsModule)},
+            ]},
+
+            // 404 & Catch all
+            {path: '404-not-found', pathMatch: 'full', loadChildren: () => import('app/modules/admin/pages/error/error-404/error-404.module').then(m => m.Error404Module)},
+            {path: '**', redirectTo: '404-not-found'}
+        ]
+    }
+];
---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,222 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id      : 'dashboards',
+        title   : 'Dashboards',
+        subtitle: 'Unique dashboard designs',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [
+            {
+                id   : 'dashboards.project',
+                title: 'Project',
+                type : 'basic',
+                icon : 'heroicons_outline:clipboard-check',
+                link : '/dashboards/project'
+            },
+            {
+                id   : 'dashboards.analytics',
+                title: 'Analytics',
+                type : 'basic',
+                icon : 'heroicons_outline:chart-pie',
+                link : '/dashboards/analytics'
+            }
+        ]
+    },
+    {
+        id      : 'apps',
+        title   : 'Applications',
+        subtitle: 'Everything we have is here',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [
+            {
+                id   : 'apps.academy',
+                title: 'Presale',
+                type : 'basic',
+                icon : 'heroicons_outline:shopping-bag',
+                link : '/apps/presale'
+            },
+            {
+                id   : 'pages.activities',
+                title: 'Presale Schedule',
+                type : 'basic',
+                icon : 'heroicons_outline:trending-up',
+                link : '/pages/presaleschedule'
+            },
+            {
+                id   : 'apps.contacts',
+                title: 'History Transaction',
+                type : 'basic',
+                icon : 'heroicons_outline:collection',
+                link : '/apps/transaction'
+            },
+            {
+                id      : 'apps.help-center',
+                title   : 'Help center',
+                type    : 'collapsable',
+                icon    : 'heroicons_outline:support',
+                link    : '/apps/help-center',
+                children: [
+                    {
+                        id        : 'apps.help-center.home',
+                        title     : 'Home',
+                        type      : 'basic',
+                        link      : '/apps/help-center',
+                        exactMatch: true
+                    },
+                    {
+                        id   : 'apps.help-center.faqs',
+                        title: 'FAQs',
+                        type : 'basic',
+                        link : '/apps/help-center/faqs'
+                    },
+                    {
+                        id   : 'apps.help-center.guides',
+                        title: 'Guides',
+                        type : 'basic',
+                        link : '/apps/help-center/guides'
+                    },
+                    {
+                        id   : 'apps.help-center.support',
+                        title: 'Support',
+                        type : 'basic',
+                        link : '/apps/help-center/support'
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        id      : 'pages',
+        title   : 'Profile',
+        subtitle: 'Custom Your Profile in page',
+        type    : 'group',
+        icon    : 'heroicons_outline:document',
+        children: [
+            {
+                id   : 'pages.settings',
+                title: 'Settings',
+                type : 'basic',
+                icon : 'heroicons_outline:cog',
+                link : '/pages/settings'
+            }
+        ]
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id      : 'dashboards',
+        title   : 'Dashboards',
+        type    : 'aside',
+        icon    : 'heroicons_outline:home',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'apps',
+        title   : 'Apps',
+        type    : 'aside',
+        icon    : 'heroicons_outline:qrcode',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'pages',
+        title   : 'Pages',
+        type    : 'aside',
+        icon    : 'heroicons_outline:document-duplicate',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'user-interface',
+        title   : 'UI',
+        type    : 'aside',
+        icon    : 'heroicons_outline:collection',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'navigation-features',
+        title   : 'Navigation',
+        type    : 'aside',
+        icon    : 'heroicons_outline:menu',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id      : 'dashboards',
+        title   : 'DASHBOARDS',
+        type    : 'group',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'apps',
+        title   : 'APPS',
+        type    : 'group',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id   : 'others',
+        title: 'OTHERS',
+        type : 'group'
+    },
+    {
+        id      : 'pages',
+        title   : 'Pages',
+        type    : 'aside',
+        icon    : 'heroicons_outline:document-duplicate',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'user-interface',
+        title   : 'User Interface',
+        type    : 'aside',
+        icon    : 'heroicons_outline:collection',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'navigation-features',
+        title   : 'Navigation Features',
+        type    : 'aside',
+        icon    : 'heroicons_outline:menu',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id      : 'dashboards',
+        title   : 'Dashboards',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'apps',
+        title   : 'Apps',
+        type    : 'group',
+        icon    : 'heroicons_outline:qrcode',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'pages',
+        title   : 'Pages',
+        type    : 'group',
+        icon    : 'heroicons_outline:document-duplicate',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'user-interface',
+        title   : 'UI',
+        type    : 'group',
+        icon    : 'heroicons_outline:collection',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'navigation-features',
+        title   : 'Misc',
+        type    : 'group',
+        icon    : 'heroicons_outline:menu',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    }
+];
